7d5a845b358e90de82e1b25bf0fda76053890720
shi.li <shi.li@thebeastshop.com>
2017-02-15 23:04:20 +0800

    start write

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,11 +1,8 @@
 Page({
     onTap: function (event) {
-        // wx.navigateTo({
-        //     url:"../posts/post"
-        // });
         
         wx.switchTab({
-            url: "../posts/post"
+           
         });
         
     }
