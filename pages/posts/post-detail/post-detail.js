065dd49a7350a54ae527d598c5496eb70c615efb
shi.li <shi.li@thebeastshop.com>
2017-03-08 00:37:22 +0800

    r

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -4,6 +4,36 @@ Page({
     onLoad: function (options) {
         var postId = options.id;
         var postData = postsData.postList[postId];
-        this.setData({postData});
+        this.setData({postData,postId});
+
+        // var postsCollected = {
+        //     1:"true",
+        //     2:"false",
+        //     3:"true"
+        // }
+
+        var postsCollected = wx.getStorageSync('posts_collected');
+        if (postsCollected) {
+            var postCollected = postsCollected[postId];
+            this.setData({ collected: postCollected });
+        } else {
+            var postsCollected = {};
+            postsCollected[postId] = false;
+            wx.setStorageSync('posts_collected', postsCollected);
+        }
+    },
+
+    onColletionTap: function(event){
+        var postsCollected = wx.getStorageSync('posts_collected');
+        var postCollected = postsCollected[this.data.postId];
+        postCollected = !postCollected;
+        postsCollected[this.data.postId] = postCollected;
+        // 更新文章是否缓存
+        wx.setStorageSync('posts_collected',postsCollected);
+        // 更新数据绑定变量，从而实现图片切换
+        this.setData({collected:postCollected})
+
+        console.log("lishi : " + this.data.postId)        
     }
+    
 })
